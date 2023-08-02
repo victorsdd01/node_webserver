32891fa212becf9b084ac97f356bdbbfaa899c10
Victor Manuel Gonzalez Del Vasto <vgonzalez@jpglobaldigital.com>
2023-08-02 08:24:29 -0700

    many changes added

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.set('view engine', 'hbs');
+
+// middleware
+app.use( express.static('public') )
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    nombre: 'Victor',
+    titulo: 'Curso de node'
+  })
+})
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    nombre: 'Victor',
+    titulo: 'Curso de node'
+  })
+})
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    nombre: 'Victor',
+    titulo: 'Curso de node'
+  })
+})
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(8080)
